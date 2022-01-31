b5169179c11a92361114b33e2e766e77ffc81b85
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-31 02:46:14 -0300

    Add files via upload

--- funcao_maximo_divisor_comum.R
+++ funcao_maximo_divisor_comum.R
@@ -1,25 +1,24 @@
 num_prim <- function(x, y) {
   if (is.numeric(x) && is.numeric(y))
   {
-    a <- c(2, 3, 5, 7, 9)
-    a
-    b <- c(2, 3, 5, 7, 9)
+    a <- c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+    b <- c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
     for (i in x)
     {
       for (j in y)
         {
           x <- (i/a);
-          if ((x) == 320) break ;
+          if ((x) == 92.90323) break ;
           print(i) | print(a) | print(x) ;
           print("######");
           y <- (j/b);
+          if ((y) == 116.12903) break ; 
           print(j) | print(b) |  print(y);
-          if ((y) == 400) break ; 
       }
     }
       }
 }
-
+   
 num_prim(2880, 3600)
 
-## Desafio: qual o número primo divisor de x (2880) e y (3600) que resultam em 320 e 400, respectivamente?
+## Desafio: 
